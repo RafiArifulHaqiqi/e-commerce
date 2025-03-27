14bc247fbab8c40613dc9b092b8d443b6a068286
RafiArifulHaqiqi <raffihaqiqi@gmail.com>
2025-03-27 12:15:10 +0700

    [Pertemuan 3] Membuat Layout dan Component

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,81 @@
 <?php
 
-Route::get('/', function () {
-    return 'Halaman Utama';
-});
+    use Illuminate\Support\Facades\Route;
+    use Livewire\Volt\Volt;
 
-Route::get('/produk', function () {
-    return 'Halaman Produk';
-});
+    Route::get('/', function () {
+        return('Halaman utama');
+    });
+    // Route::get('/', function () {
+    //     return view('web.homepage');
+    // });
+    
+    Route::get('/', function () {
+        $title = "Homepage";
+        return view('web.homepage', ['title' => $title]);
+    });
+    
+    Route::get('products', function () {
+        $title = "Products";
+        return view("web.products", ['title' => $title]);
+    });
+    
+    Route::get('product/{slug}', function ($slug) {
+        return "halaman single product - " . $slug;
+    });
+    
+    Route::get('categories', function () {
+        return view("web.categories");
+    });
+    
+    Route::get('category/{slug}', function ($slug) {
+        return "halaman single category - " . $slug;
+    });
+    
+    Route::get('cart', function () {
+        $title = "Cart";
+        return view("web.cart", ['title' => $title]);
+    });
+    
+    Route::get('checkout', function () {
+        $title = "co";
+        return view("web.checkout", ['title' => $title]);
+    });
 
-Route::get('/produk/{id}', function ($id) {
-    return "Halaman Detail Produk dengan ID: $id";
-});
+// Route::get('/', function(){
+//     $title = "Homepage - Toko Butik";
+//     return view('web.homepage',['title'=>$title]);
+//    });
+//    Route::get('products', function(){
+//     $title = "Products - Gamis";
 
-Route::get('/keranjang', function () {
-    return 'Halaman Keranjang Belanja';
-});
+//     return view('web.products',['title'=>$title]);
+//    });
+//    Route::get('product/{slug}', function($slug){
+//     $title = "Single Product";
 
-Route::get('/checkout', function () {
-    return 'Halaman Checkout';
-});
+//     return view('web.single_product',['title'=>$title,'slug'=>$slug]);
+//    });
+//    Route::get('categories', function(){
+//     $title = "Categories";
 
-Route::get('/login', function () {
-    return 'Halaman Login';
-});
+//     return view('web.categories',['title'=>$title]);
+//    });
+//    Route::get('category/{slug}', function($slug){
+//     $title = "Single Category";
 
-Route::get('/register', function () {
-    return 'Halaman Registrasi';
-});
+//     return view('web.single_category',['title'=>$title,'slug'=>$slug]);
+//    });
+//    Route::get('cart', function(){
+//     $title = "Cart";
 
-Route::get('/profil', function () {
-    return 'Halaman Profil Pengguna';
-});
+//     return view('web.cart',['title'=>$title]);
+//    });
+//    Route::get('checkout', function(){
+//     $title = "Checkout";
+    
+//     return view('web.checkout',['title'=>$title]);
+//    });
 
 // use Illuminate\Support\Facades\Route;
 // use Livewire\Volt\Volt;
